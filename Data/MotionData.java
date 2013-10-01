2dce56b13f42745c7866e13663bd303c1b1a8f51
FumihikoKouno <2nvb.b4k@gmail.com>
2013-10-01 21:17:33 +0900

    add convert functions to MotionData.java

--- Data/MotionData.java
+++ Data/MotionData.java
@@ -3,7 +3,6 @@ package Tools.Data;
 import java.util.ArrayList;
 
 public class MotionData{
-
     public static final int TORSO = 0;
     public static final int NECK = 1;
     public static final int HEAD = 2;
@@ -25,27 +24,59 @@ public class MotionData{
     private ArrayList<Vec3D[]> data;
 
     public MotionData(){}
+    
     public void add(Vec3D[] a){
-	data.add(a);
+        data.add(a);
     }
     
     public int size(){
-	return data.size();
+        return data.size();
     }
 
     public Vec3D get(int time, int position){
-	if(time >= data.size() || position >= JOINT_NUMBER) return null;
-	else return data.get(time)[position];
+        if(time >= data.size() || position >= JOINT_NUMBER) return null;
+        else return data.get(time)[position];
+    }
+    
+    public Vec3D[] convert(MotionData model, int i){
+        if(empty() || model.empty()){ return null; }
+        Vec3D diff = new Vec3D(get(0,TORSO).sub(model.get(0,TORSO)));
+        Vec3D[] vec = new Vec3D[JOINT_NUMBER];
+        vec[TORSO] = get(i,TORSO).add(diff);
+        vec[NECK]       = makeNext(model,i, TORSO,      NECK);
+        vec[HEAD]       = makeNext(model,i, NECK,       HEAD);
+        vec[R_SHOULDER] = makeNext(model,i, TORSO,      R_SHOULDER);
+        vec[R_ELBOW]    = makeNext(model,i, R_SHOULDER, R_ELBOW);
+        vec[R_HAND]     = makeNext(model,i, R_ELBOW,    R_HAND);
+        vec[L_SHOULDER] = makeNext(model,i, TORSO,      L_SHOULDER);
+        vec[L_ELBOW]    = makeNext(model,i, L_SHOULDER, L_ELBOW);
+        vec[L_HAND]     = makeNext(model,i, L_ELBOW,    L_HAND);
+        vec[R_HIP]      = makeNext(model,i, TORSO,      R_HIP);
+        vec[R_KNEE]     = makeNext(model,i, R_HIP,      R_KNEE);
+        vec[R_FOOT]     = makeNext(model,i, R_KNEE,     R_FOOT);
+        vec[L_HIP]      = makeNext(model,i, TORSO,      L_HIP);
+        vec[L_KNEE]     = makeNext(model,i, L_HIP,      L_KNEE);
+        vec[L_FOOT]     = makeNext(model,i, L_KNEE,     L_FOOT);
+        return vec;
+    }
+    
+    public boolean empty(){
+        return data.size() == 0;
     }
 
-    public MotionData convert(MotionData model){
-	
+    public MotionData convertAll(MotionData model){
+        MotionData ret = new MotionData();
+        int end = Math.min(model.size(),size());
+        for(int i = 0; i < end; i++){
+            ret.add(convert(model,i));
+        }
+        return ret;
     }
 
-    private Vec3D makeNext(Vec3D[] model, int from, int to){
-	Vec3D ret = model[to]-model[from];
-	ret = ret.times(1/ret.abs());
-	ret.times(1);
+    private Vec3D makeNext(MotionData model, int t, int from, int to){
+        Vec3D ret = model.get(t,to).sub(model.get(t,from));
+        ret = ret.times(1.0/ret.abs());
+        return get(t,from).add(ret.times((get(t,to).sub(get(t,from)).abs())));
     }
 
 }
