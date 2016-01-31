380841137b744fa36f3c23e60628fba4f3a5a422
FumihikoKouno <fumihikokouno@com>
2016-01-31 13:22:07 +0900

    add main

---
+++ MyShell/main.py
@@ -0,0 +1,21 @@
+import argparse
+import myshell
+
+arg_parser = argparse.ArgumentParser()
+arg_parser.add_argument('-p', '--prefix', 
+                        help='Specify prefix character.: default "$"')
+arg_parser.add_argument('-t', '--time',
+                        action='store_true',
+                        help='Print command input time.')
+arg_parser.add_argument('-e', '--elapsed',
+                        action='store_true',
+                        help='Print command execute time.')
+arg_parser.add_argument('log_file')
+args = arg_parser.parse_args()
+
+shell = myshell.LoggingShell(args.log_file,
+                             prefix=args.prefix,
+                             time=args.time,
+                             elapsed=args.elapsed)
+
+shell.run()
