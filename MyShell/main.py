ddc803b93978023a15799df164dda2bf64edb6cb
FumihikoKouno <fumihikokouno@com>
2016-01-31 23:38:26 +0900

    add completion

--- MyShell/main.py
+++ MyShell/main.py
@@ -1,16 +1,19 @@
+#!/usr/bin/env python
+
 import argparse
 import myshell
 
 arg_parser = argparse.ArgumentParser()
 arg_parser.add_argument('-p', '--prefix', 
-                        help='Specify prefix character.: default "$"')
+                        help='Specify prefix character. default: "$"')
 arg_parser.add_argument('-t', '--time',
                         action='store_true',
                         help='Print command input time.')
 arg_parser.add_argument('-e', '--elapsed',
                         action='store_true',
                         help='Print command execute time.')
-arg_parser.add_argument('log_file')
+arg_parser.add_argument('log_file',
+                        help='Logging file name')
 args = arg_parser.parse_args()
 
 shell = myshell.LoggingShell(args.log_file,
